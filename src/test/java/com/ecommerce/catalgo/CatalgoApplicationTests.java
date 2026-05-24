ef6693e0fa4034acd1f9336f90324227ba16937c
Joseph Paredes <alejitoparedes2006@gmail.com>
2026-05-24 00:12:54 -0500

    Creacion de arquitectura

---
+++ src/test/java/com/ecommerce/catalgo/CatalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.catalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
